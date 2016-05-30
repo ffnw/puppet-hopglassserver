56965bc5f0a644fb5eb43ed3effa74fe8dc75810
Simon Kurka <dev@simon.kurka.cc>
2016-05-30 23:05:38 +0200

    added path for bootstrap script to work

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,8 +2,9 @@ class hopglassserver::install inherits hopglassserver {
 
   exec { 'bootstrap hopglass-server':
     cwd     => '/tmp',
-    command => '/usr/bin/wget https://raw.githubusercontent.com/hopglass/hopglass-server/v0.1.1/scripts/bootstrap.sh && /bin/bash bootstrap.sh && /bin/rm bootstrap.sh',
+    command => 'wget https://raw.githubusercontent.com/hopglass/hopglass-server/v0.1.1/scripts/bootstrap.sh && bash bootstrap.sh && rm bootstrap.sh',
     unless  => '/usr/bin/test -f /opt/hopglass/server',
+    path    => [ '/bin/','/sbin/','/usr/bin/','/usr/sbin/' ],
   }
 
 }
