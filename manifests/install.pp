152e2e88bef022fd4b600c314537aa2f90e33490
Simon Kurka <dev@simon.kurka.cc>
2016-05-31 01:43:51 +0200

    test for directory

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,7 +3,7 @@ class hopglassserver::install inherits hopglassserver {
   exec { 'bootstrap hopglass-server':
     cwd     => '/tmp',
     command => 'wget https://raw.githubusercontent.com/hopglass/hopglass-server/v0.1.1/scripts/bootstrap.sh && sed "s;pidof systemd;pgrep -x systemd;g" -i /tmp/bootstrap.sh && bash bootstrap.sh; rm bootstrap.sh',
-    unless  => '/usr/bin/test -f /opt/hopglass/server',
+    unless  => '/usr/bin/test -d /opt/hopglass/server',
     path    => [ '/bin/','/sbin/','/usr/bin/','/usr/sbin/' ],
   }
 
