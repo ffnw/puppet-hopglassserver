a5d0bb4d2abd56dfa10d8a92203108a67a42c9f7
Simon Kurka <dev@simon.kurka.cc>
2016-05-30 22:57:12 +0200

    use full qualified paths

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,7 @@ class hopglassserver::install inherits hopglassserver {
 
   exec { 'bootstrap hopglass-server':
     cwd     => '/tmp',
-    command => 'wget https://raw.githubusercontent.com/hopglass/hopglass-server/v0.1.1/scripts/bootstrap.sh && bash bootstrap.sh && rm bootstrap.sh',
+    command => '/usr/bin/wget https://raw.githubusercontent.com/hopglass/hopglass-server/v0.1.1/scripts/bootstrap.sh && /bin/bash bootstrap.sh && /bin/rm bootstrap.sh',
     unless  => '/usr/bin/test -f /opt/hopglass/server',
   }
 
