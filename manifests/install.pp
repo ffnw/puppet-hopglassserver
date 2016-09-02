8ed3c00fa5e05c3bbb33a2818a68de897abc62e6
Simon Kurka <dev@simon.kurka.cc>
2016-09-03 00:28:12 +0200

    always use master because releases are slow in coming

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,23 @@
 class hopglassserver::install inherits hopglassserver {
 
+  if !defined(Package['git']) {
+    package { 'git':
+      ensure => installed,
+    }
+  }
+
+  vcsrepo { '/opt/hopglass-server':
+    ensure   => latest,
+    owner    => 'root',
+    group    => 'root',
+    provider => 'git',
+    require  => [ Package['git'] ],
+    source   => 'https://github.com/hopglass/hopglass-server.git',
+    revision => 'master',
+  } ~>
   exec { 'bootstrap hopglass-server':
-    cwd     => '/tmp',
-    command => 'wget https://raw.githubusercontent.com/hopglass/hopglass-server/v0.1.1/scripts/bootstrap.sh && sed "s;pidof systemd;pgrep -x systemd;g" -i /tmp/bootstrap.sh && bash bootstrap.sh; rm bootstrap.sh',
-    unless  => '/usr/bin/test -d /opt/hopglass/server',
+    cwd     => '/opt/hopglass-server',
+    command => 'bash bootstrap.sh',
     path    => [ '/bin/','/sbin/','/usr/bin/','/usr/sbin/' ],
   }
 
