878453d928aada797290148909232595dd63d88b
Simon Kurka <dev@simon.kurka.cc>
2016-09-03 02:06:17 +0200

    always use git master and npm install on update

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,19 +6,27 @@ class hopglassserver::install inherits hopglassserver {
     }
   }
 
-  vcsrepo { '/opt/hopglass-server':
+  exec { 'bootstrap hopglass-server':
+    cwd     => '/tmp',
+    command => 'wget https://raw.githubusercontent.com/hopglass/hopglass-server/v0.1.1/scripts/bootstrap.sh && sed "s;^install$;;g" -i bootstrap.sh && sed "s;^ask_user$;;g" -i bootstrap.sh && sed "s;pidof systemd;pgrep -x systemd;g" -i /tmp/bootstrap.sh && bash bootstrap.sh; rm bootstrap.sh',
+    unless  => '/usr/bin/test -d /opt/hopglass/server',
+    path    => [ '/bin/','/sbin/','/usr/bin/','/usr/sbin/' ],
+  }
+
+  vcsrepo { '/opt/hopglass/server':
     ensure   => latest,
-    owner    => 'root',
-    group    => 'root',
+    owner    => 'hopglass',
+    group    => 'hopglass',
     provider => 'git',
     require  => [ Package['git'] ],
     source   => 'https://github.com/hopglass/hopglass-server.git',
     revision => 'master',
   } ~>
-  exec { 'bootstrap hopglass-server':
-    cwd     => '/opt/hopglass-server/scripts',
-    command => 'bash bootstrap.sh',
-    path    => [ '/bin/','/sbin/','/usr/bin/','/usr/sbin/' ],
+  exec { 'hopglassserver npm install':
+    cwd         => '/opt/hopglass/server',
+    command     => 'npm install',
+    path        => [ '/bin/','/sbin/','/usr/bin/','/usr/sbin/' ],
+    refreshonly => true,
   }
 
 }
