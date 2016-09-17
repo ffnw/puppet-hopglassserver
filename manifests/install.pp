8b965fccbb206856e9ddc6ea80f6ec0da026c382
Simon Kurka <dev@simon.kurka.cc>
2016-09-17 13:37:50 +0200

    obsoleting bootstrap

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,16 +1,19 @@
 class hopglassserver::install inherits hopglassserver {
 
-  if !defined(Package['git']) {
-    package { 'git':
-      ensure => installed,
+  ["nodejs","git"].each | $value | {
+    if(!defined(Package[$value])) {
+      package { $value:
+        ensure => installed,
+      }
     }
   }
 
-  exec { 'bootstrap hopglass-server':
-    cwd     => '/tmp',
-    command => 'wget https://raw.githubusercontent.com/hopglass/hopglass-server/v0.1.1/scripts/bootstrap.sh && sed "s;^install$;;g" -i bootstrap.sh && sed "s;^ask_user$;;g" -i bootstrap.sh && sed "s;pidof systemd;pgrep -x systemd;g" -i /tmp/bootstrap.sh && bash bootstrap.sh; rm bootstrap.sh',
-    unless  => '/usr/bin/test -d /opt/hopglass/server',
-    path    => [ '/bin/','/sbin/','/usr/bin/','/usr/sbin/' ],
+  user { 'hopglass':
+    ensure => present,
+    system => true,
+    shell  => '/bin/false',
+    home   => '/opt/hopglass',
+    group  => "${title}"
   } ->
   vcsrepo { '/opt/hopglass/server':
     ensure   => latest,
