5774100f42f4cf5625487f3656cfe52f0aa1f5aa
Simon Kurka <dev@simon.kurka.cc>
2016-09-17 13:10:49 +0200

    Ordering bootstrap

--- manifests/install.pp
+++ manifests/install.pp
@@ -11,8 +11,7 @@ class hopglassserver::install inherits hopglassserver {
     command => 'wget https://raw.githubusercontent.com/hopglass/hopglass-server/v0.1.1/scripts/bootstrap.sh && sed "s;^install$;;g" -i bootstrap.sh && sed "s;^ask_user$;;g" -i bootstrap.sh && sed "s;pidof systemd;pgrep -x systemd;g" -i /tmp/bootstrap.sh && bash bootstrap.sh; rm bootstrap.sh',
     unless  => '/usr/bin/test -d /opt/hopglass/server',
     path    => [ '/bin/','/sbin/','/usr/bin/','/usr/sbin/' ],
-  }
-
+  } ->
   vcsrepo { '/opt/hopglass/server':
     ensure   => latest,
     owner    => 'hopglass',
